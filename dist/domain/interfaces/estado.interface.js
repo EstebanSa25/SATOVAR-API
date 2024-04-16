7a0c40cff9a3936a3a343344470cd009036a944d
Esteban Salas <estebansalas2502@gmail.com>
2024-04-16 14:48:32 -0600

    produccion 2

--- dist/domain/interfaces/estado.interface.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Estado = void 0;
-var Estado;
-(function (Estado) {
-    Estado[Estado["Pendiente"] = 1] = "Pendiente";
-    Estado[Estado["Completo"] = 2] = "Completo";
-    Estado[Estado["enProceso"] = 3] = "enProceso";
-})(Estado || (exports.Estado = Estado = {}));
